0c08edea41854ed383142a90254bb5aa98caa0ac
Maxwell Kendall <Maxwell.N.Kendall@gmail.com>
2023-09-02 12:10:20 -0400

    mocha w chai

--- test/app.test.js
+++ test/app.test.js
@@ -1,23 +1,15 @@
-import assert from 'assert';
+// https://www.chaijs.com/api/bdd/
+import { assert, expect, should } from 'chai';
 
 import { add } from '../src/app.js';
 
 describe('add fn', () => {
     it('should add 1 + 2', () => {
         const result = add(1, 2);
-        assert.strictEqual(result, 3);        
+        
+        assert.equal(result, 3);
+        expect(result).to.equal(3);        
+        should().equal(result, 3);
     });
 })
-
-/**
- * Mocha Features
- * 1. Nested Describe Statements (or use context())
- * 2. Testing Async Code: https://mochajs.org/#hooks
- * 3. Hooks: https://mochajs.org/#hooks 
- * 4. Configuration
- *  a. Timeouts
- *  b. Reporting "Slow" tests
- *  c. Parallelism
- * 5. Pending Tests: https://mochajs.org/#pending-tests
- */
 
